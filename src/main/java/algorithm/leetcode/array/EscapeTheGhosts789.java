3507aba4e381b18a463ca3209f62c5c3ebec4774
xiongyayun <18850198317@163.com>
2021-08-22 21:19:32 +0800

    fix:leetcode 每日一题

---
+++ src/main/java/algorithm/leetcode/array/EscapeTheGhosts789.java
@@ -0,0 +1,26 @@
+package algorithm.leetcode.array;
+
+/**
+ * @author: xiongyayun
+ * @date: 2021/8/22 9:19 下午
+ */
+public class EscapeTheGhosts789 {
+
+    public boolean escapeGhosts(int[][] ghosts, int[] target) {
+        int[] source = {0, 0};
+        int distance = manhattanDistance(source, target);
+        for (int[] ghost : ghosts) {
+            int ghostDistance = manhattanDistance(ghost, target);
+            if (ghostDistance <= distance) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public int manhattanDistance(int[] point1, int[] point2) {
+        return Math.abs(point1[0] - point2[0]) + Math.abs(point1[1] - point2[1]);
+    }
+
+
+}
