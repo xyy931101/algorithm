46fde2f17870d28116792bea7bb16728654808f2
xiongyayun <18850198317@163.com>
2021-11-02 08:32:12 +0800

    fix:leetcode 每日一题

---
+++ src/main/java/algorithm/leetcode/linkedList/DeletenodeInALinkedList237.java
@@ -0,0 +1,14 @@
+package algorithm.leetcode.linkedList;
+
+/**
+ * 237. 删除链表中的节点
+ * @author: xiongyayun
+ * @date: 2021/11/2 8:29
+ */
+public class DeletenodeInALinkedList237 {
+
+    public void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+}
