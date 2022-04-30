43f4e14bd0ea335b599e104c06ac39f5bd1d946b
xyy931101 <18850198317@163.com>
2022-04-30 22:32:39 +0800

    fix:leetcode

---
+++ src/main/java/algorithm/leetcode/array/FindKClosestElements658.java
@@ -0,0 +1,42 @@
+package algorithm.leetcode.array;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * 658. 找到 K 个最接近的元素
+ * @author: xiongyayun
+ * @date: 2022/4/30 22:23
+ */
+public class FindKClosestElements658 {
+
+    public static void main(String[] args) {
+        System.out.println(findClosestElements(new int[]{1, 2, 3, 4, 5}, 4, 3));
+    }
+
+    public static List<Integer> findClosestElements(int[] arr, int k, int x) {
+        int size = arr.length;
+
+        int left = 0;
+        int right = size - k;
+
+        while (left < right) {
+            // int mid = left + (right - left) / 2;
+            int mid = (left + right) >>> 1;
+            // 尝试从长度为 k + 1 的连续子区间删除一个元素
+            // 从而定位左区间端点的边界值
+            if (x - arr[mid] > arr[mid + k] - x) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+
+        List<Integer> res = new ArrayList<>();
+        for (int i = left; i < left + k; i++) {
+            res.add(arr[i]);
+        }
+        return res;
+    }
+}
